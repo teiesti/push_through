3e6aa96967ffe27430e9f67f5e11eed3c2bf25a5
Tobias Stolzmann <tobias.stolzmann@gmail.com>
2022-02-06 19:02:43 +0100

    Implement loading a configuration file

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,25 @@
+mod config;
 mod deploy;
 
-use {env_logger::Env, log::info};
+use {
+    crate::config::Configuration,
+    anyhow::{Error, Result},
+    env_logger::Env,
+    log::error,
+    log::info,
+};
 
 const PKG_NAME: &str = env!("CARGO_PKG_NAME");
 const PKG_VERSION: &str = env!("CARGO_PKG_VERSION");
 const PKG_COMMIT: &str = env!("PKG_COMMIT");
 
 fn main() {
+    if let Err(err) = try_main() {
+        handle_error(err);
+    }
+}
+
+fn try_main() -> Result<()> {
     // Initialize logging
     let env = Env::default().default_filter_or(format!("{}=info", PKG_NAME));
     env_logger::init_from_env(env);
@@ -14,5 +27,14 @@ fn main() {
     // Log the version
     info!("Starting {} {} ({})", PKG_NAME, PKG_VERSION, PKG_COMMIT);
 
+    // Load the configuration
+    let configuration = Configuration::discover()?;
+
     // TODO
+    info!("{:?}", configuration);
+    Ok(())
+}
+
+fn handle_error(err: Error) {
+    error!("{:?}", err);
 }
