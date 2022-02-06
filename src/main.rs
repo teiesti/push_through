17a423eaa9e41d1966c497678a2ec8f61d42b100
Tobias Stolzmann <tobias.stolzmann@gmail.com>
2022-02-06 17:55:35 +0100

    Add basic logging

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use {
+    env_logger::Env,
+    log::info,
+};
+
+const PKG_NAME: &str = env!("CARGO_PKG_NAME");
+const PKG_VERSION: &str = env!("CARGO_PKG_VERSION");
+const PKG_COMMIT: &str = env!("PKG_COMMIT");
+
 fn main() {
-    println!("Hello, world!");
+    // Initialize logging
+    let env = Env::default().default_filter_or(format!("{}=info", PKG_NAME));
+    env_logger::init_from_env(env);
+
+    // Log the version
+    info!("Starting {} {} ({})", PKG_NAME, PKG_VERSION, PKG_COMMIT);
+
+    // TODO
 }
